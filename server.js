699d12335cce251c6b38dda62b096a3e43aefee1
michealkusi42-bit <michealkusi42@gmail.com>
2026-06-12 03:27:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ io.on('connection', (socket) => {
         });
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-  console.log(🚀 Foretell Backend running on port ${PORT});
+  console.log('🚀 Foretell Backend running on port ${PORT'});
   // initGames(io);
 });
 
